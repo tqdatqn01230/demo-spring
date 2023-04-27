1f3203e0e5be78609ace547f02c81562838f58a7
Darrow Trinh <tqdatqn01230@gmail.com>
2023-04-27 23:53:49 +0700

    first commit

---
+++ src/main/java/com/securitytest/darrow/DarrowApplication.java
@@ -0,0 +1,13 @@
+package com.securitytest.darrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitytest/darrow/DarrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitytest.darrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
